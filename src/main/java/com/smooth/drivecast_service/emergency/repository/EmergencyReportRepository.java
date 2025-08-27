f2378706838a5115e753b1f323221034aa5b10f8
Jiyoung Park <pjy2163@naver.com>
2025-08-27 09:56:13 +0900

    feat: 응급 신고 처리 서비스, 레파지토리 구현

---
+++ src/main/java/com/smooth/drivecast_service/emergency/repository/EmergencyReportRepository.java
@@ -0,0 +1,15 @@
+package com.smooth.drivecast_service.emergency.repository;
+
+import com.smooth.drivecast_service.emergency.entity.EmergencyReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmergencyReportRepository extends JpaRepository<EmergencyReport, Long> {
+
+    boolean existsByAccidentIdAndUserIdAndEmergencyNotifiedTrue(Long accidentId, Long userId);
+
+    List<EmergencyReport> findByUserIdOrderByReportTimeDesc(Long userId);
+}

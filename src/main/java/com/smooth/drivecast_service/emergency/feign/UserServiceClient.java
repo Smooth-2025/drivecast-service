2be1cb3d03bf0419f7890821afec3b2ae1dca6a9
Jiyoung Park <pjy2163@naver.com>
2025-08-29 10:41:20 +0900

    refactor: 환경변수 url로 변경

--- src/main/java/com/smooth/drivecast_service/emergency/feign/UserServiceClient.java
+++ src/main/java/com/smooth/drivecast_service/emergency/feign/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081", path = "internal/v1/users", fallback = UserServiceClientFallback.class)
+@FeignClient(name = "user-service", url = "${app.client.user-service.url}", path = "internal/v1/users", fallback = UserServiceClientFallback.class)
 public interface UserServiceClient {
     @GetMapping("/{userId}/emergency-info")
     EmergencyInfoResponse getUserInfo(@PathVariable("userId") String userId);

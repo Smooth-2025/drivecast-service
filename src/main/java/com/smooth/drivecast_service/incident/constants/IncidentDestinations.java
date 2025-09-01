da6b6e7b3d2bc5030313af0d234c08ecadec7c5f
damiiya <ddam2lee@gmail.com>
2025-09-01 11:10:13 +0900

    Revert "Refactor dev"

--- src/main/java/com/smooth/drivecast_service/incident/constants/IncidentDestinations.java
+++
@@ -1,8 +0,0 @@
-package com.smooth.drivecast_service.incident.constants;
-
-/**
- * Incident(사고) 도메인의 STOMP 목적지(DESTINATION) 상수
- **/
-public class IncidentDestinations {
-    public static final String INCIDENT_ALERT = "/queue/incident";
-}

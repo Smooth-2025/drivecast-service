5d92a155e795646af11eba1fbc3e82d988d9aaf1
Jiyoung Park <pjy2163@naver.com>
2025-09-06 00:28:08 +0900

    refactor: 관리자 사고 조회용 dto, controller 수정

--- src/main/java/com/smooth/drivecast_service/emergency/repository/EmergencyReportRepository.java
+++ src/main/java/com/smooth/drivecast_service/emergency/repository/EmergencyReportRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmergencyReportRepository extends JpaRepository<EmergencyReport, Long> {
 
-    boolean existsByAccidentIdAndUserIdAndEmergencyNotifiedTrue(String accidentId, Long userId);
-    
     boolean existsByAccidentIdAndUserId(String accidentId, Long userId);
 
     List<EmergencyReport> findByUserIdOrderByReportTimeDesc(Long userId);
+
+    Optional<EmergencyReport> findByAccidentId(String accidentId);
 }
